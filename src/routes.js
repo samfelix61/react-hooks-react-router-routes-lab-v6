93f94cdfab1a5f804350121681a09d64bf782f53
samfelix61 <samfelix61@gmail.com>
2024-05-04 23:33:25 +0300

    complete

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,27 @@
-
-
+import React from 'react';
+import Home from './pages/Home';
+import Directors from './pages/Directors';
+import Movie from './pages/Movie';
+import Actors from './pages/Actors';
+import ErrorPage from './pages/ErrorPage';
 const routes = [
+  {
+    path: '/',
+    element: <Home />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: '/directors',
+    element: <Directors />
+  },
+  {
+    path: '/movie/:id',
+    element: <Movie />
+  },
+  {
+    path: '/actors',
+    element: <Actors />
+  } 
    
   ];
 
